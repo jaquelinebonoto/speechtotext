9bb9b47a6bdc0cfa72c4dd4a46243b2258d2e9f2
jaquelinebonoto <39954954+jaquelinebonoto@users.noreply.github.com>
2020-03-16 10:35:41 -0300

    Adicionando lógica de funcionamento Google Speech

--- app/src/main/java/com/ssi/audiototext/MainActivity.java
+++ app/src/main/java/com/ssi/audiototext/MainActivity.java
@@ -1,13 +1,59 @@
 package com.ssi.audiototext;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    //inicializando o espaço para o texto retornar
+    private TextView txtResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtResult = (TextView) findViewById(R.id.txvResult);
+    }
+
+    //ação que está associada a imagem do microfone
+    public void getSpeechInput(View view) {
+
+        //inicia uma acitivity que ira pegar o speech do usuario e enviar para o recognizer. os reultados serão
+        //retornados via resultados em uma atividade
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        //este parametro extra pegará a lingua defaut do seu android
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        //validando se o dispositivo suporta a operção
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, 10);
+        }else{
+            Toast.makeText(this, "Seu dispositivo não suporta Speech Input", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case 10:
+                if(resultCode == RESULT_OK && data != null){
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtResult.setText(result.get(0));
+                }
+                break;
+        }
     }
 }
